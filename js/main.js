244f529b9150316aa5f6ab43417f544c3976d6f1
username <stever0407@yahoo.com>
2023-07-06 15:55:33 -0700

    upgrading game functionality

--- js/main.js
+++ js/main.js
@@ -1,70 +1,29 @@
-
-
-
-
-
-
-
-
-
-/*----- constants -----*/
 const nflQuestionsAndAnswers = [
     { Athlete:'Derwin James', Details:'This NFL player plays for the Los Angeles Chargers. He came out of FSU as a safety and is regarded as one of the best defensive players in the league.'},
     { Athlete:'Tom Brady' ,Details:'This NFL player has 7 rings and most recently got his last one with the Tampa Bay Buccaneers.'  },
     { Athlete:'Patrick Mahomes' , Details:'This NFL player plays for the Kansas City Chiefs and is one of the best quarterbacks in the nfl.'},
     { Athlete:'Saquon Barkley', Details:'This NFL player is a running back out of penn state who plays for the New York Giants.' },
     { Athlete:'Joey Bosa', Details:'This NFL player came out of ohio state who plays as a defensive end and has a brother who plays the same position for the San Francisco 49ers.'},
-    { Athlete:'Justin Jefferson', Details:'This NFL player came out of LSU as a wide receiver. Currently plays for the Minnesota Vikings.'}
+    { Athlete:'Josh Allen', Details:'This NFL player came out Wyoming and currently plays for the Buffalo Bills as a quarterback.'},
+    { Athlete:'Davante Adams', Details:'This NFL player came out of Fresno State and is regarded as one of the best receivers in the NFL.'},
+    { Athlete:'Micah Parsons', Details:'This NFL player came out of Penn State. He now plays for the Dallas Cowboys as a defensive end.'},
+    { Athlete:'Jalen Ramsey', Details:'This NFL player came out of FSU and was recently traded to the Miami Dolphins. He plays as a defensive back.'},
+    { Athlete:'TJ Watt', Details:'This NFL player came out of Wisconsin as a defensive end. currently plays for the pittsburgh steelers.'},
+    { Athlete:'Von Miller', Details:'This NFL player came out of Texas A&M who currently is a defensive end for the Buffalo Bills and has also won a superbowl MVP.'},
+    { Athlete:'Aaron Donald', Details:'This NFL player came out of Pitt as a defensive tackle. He has won one superbowl.'},
+    { Athlete:'Lamar Jackson', Details:'This NFL player came out of Louisville. He plays quarterback for the Baltimore Ravens'},
+    { Athlete:'Derrick Henry', Details:'This NFL player came out of Alabama. He plays running back for the Tennessee Titans.'}
   ];
-// const nbaQuestionsAndAnswers = [
-//     { Athlete:'Lebron James', Message:'This NBA player was born in Akron Ohio and has 4 NBA rings. One of those he won with the Cleveland Cavaliers which is his home state.'},
-//     { Athlete:'Kawhi Leonard' ,Message:'This NBA player came out of San Diego State and most recently won his last NBA championship with the Toronto Raptors.'  },
-//     { Athlete:'Damien Lillard' , Message:'This NBA player played in Weber State and plays point guard for the Portland Trail Blazers.'},
-//     { Athlete:'Stephen Curry', Message:'This NBA player is a point guard who is known for his great 3 point shooting. Like Lebron he was also born in Akron Ohio' },
-//     { Athlete:'Luka Doncic', Message:'This NBA player is European and plays for the Dallas Mavericks'},
-//     { Athlete:'Kevin Durant' , Message:'This NBA player came out of texas and was drafted by the Seattle Supersonics which later turned into the OKC Thunder.'}
-//   ];
-// const mlbQuestionsAndAnswers = [
-//     { Athlete:'Shohei Ohtani', Message:'This MLB player is from Japan. He both pitches and bats and is arguably the best player in the MLB'},
-//     { Athlete:'Anthony Rendon' ,Message: 'This player bamboozled the angels out of 30 million dollars a year in their most recent dumb contract' },
-//     { Athlete:'Mike Trout' , Message:'This MLB player plays center field. He is from new jersey and is a big eagles fan. He also is regarded as one of the best players in the MLB.'},
-//     { Athlete:'Mookie Betts', Message:'This MLB player was traded from the Red Sox to the dodgers. He also won a world series with the red sox.' },
-//     { Athlete:'Bryce Harper', Message:'This MLB player is a right fielder for the Phillies. He has 2 MVPs.'},
-//     { Athlete:'Jacob Degrom', Message:'This MLB player is a pitcher who pitches for the Texas Rangers. He is currently out of the season because needed tommy john surgery.'}
-//   ];
 
 
-/*----- state variables -----*/
-let guessAthlete, userInput
 let randomIndex
+let guessedAnswer = []
 
-
-
-
-
-
-/*----- cached elements  -----*/
 
 const message = document.querySelector('h2')
 const guessInput = document.querySelector('.answer')
 
 
-
-
-
-
-/*----- event listeners -----*/
-
-
-
-
-
-
-
-
-
-
-/*----- functions -----*/
 function startGame() {
   let randomDetails = document.getElementById('detail-msg')
   randomIndex = Math.floor(Math.random() * nflQuestionsAndAnswers.length)
@@ -78,31 +37,88 @@ function startGame() {
 
 } 
 
-// After the start game function is initiated the user will get their chance to input their guess.
-
 function checkGuess () {
     let userGuess = document.getElementById('answer').value.toLowerCase()
-    // let inputElement = userGuess.value.toLowerCase()
-    //  randomIndex = Math.floor(Math.random() * nflQuestionsAndAnswers.length)
+
     let correctAnswer = nflQuestionsAndAnswers[randomIndex].Athlete.toLowerCase();
 
     document.getElementById('answer').value = ''
 
     if(userGuess === correctAnswer){
-      congrats()
-    } else {
-     incorrect() 
+      let message = document.getElementById('message')
+  message.innerHTML = 'Correct!'
+      guessedAnswer.push(randomIndex)
+      nflQuestionsAndAnswers.splice(randomIndex, 1)
+      if(nflQuestionsAndAnswers.length === 0) {
+        congrats()
+      }
+   
+    } else if (userGuess != correctAnswer){
+
+      incorrect()
     }
     
+   
 }
 
 function congrats () {
-  let message = document.getElementById('message')
-  message.innerHTML = 'Congrats! you guessed right !'
+  if(nflQuestionsAndAnswers.length === 0) {
+      let winnerMessage = document.getElementById('winning-message')
+      winnerMessage.innerHTML = 'Congrats! you have guessed everything!'
+
+      let restartButton = document.getElementById('mainButton')
+      restartButton.textContent = 'Play Game Again'
+      restartButton.addEventListener('click', function(){
+        restartGame()
+      }, {
+        once: true
+      })
+   } 
 }
 
 
 function incorrect () {
-  let message = document.getElementById('message')
-  message.innerHTML = 'Incorrect! you guessed wrong !'
+  let losingMessage = document.getElementById('losing-message')
+  losingMessage.innerHTML = 'Incorrect! Press Restart Game to play again'
+  let restartButton = document.getElementById('mainButton') 
+  restartButton.textContent = 'Restart Game'
+  restartButton.addEventListener('click', function(){
+    restartGame()
+  }, {
+    once: true
+  })
+}
+
+
+function restartGame () {
+  let restartButton = document.getElementById('mainButton') 
+  restartButton.textContent = 'Restart Game'
+  let incorrectMessage = document.getElementById('losing-message')
+  incorrectMessage.innerHTML = ''
+  let congratsMessage = document.getElementById('winning-message')
+  congratsMessage.innerHTML = ''
+
+  
+ 
+  nflQuestionsAndAnswers.length = 0
+
+  nflQuestionsAndAnswers.push(
+    { Athlete:'Derwin James', Details:'This NFL player plays for the Los Angeles Chargers. He came out of FSU as a safety and is regarded as one of the best defensive players in the league.'},
+    { Athlete:'Tom Brady' ,Details:'This NFL player has 7 rings and most recently got his last one with the Tampa Bay Buccaneers.'  },
+    { Athlete:'Patrick Mahomes' , Details:'This NFL player plays for the Kansas City Chiefs and is one of the best quarterbacks in the nfl.'},
+    { Athlete:'Saquon Barkley', Details:'This NFL player is a running back out of penn state who plays for the New York Giants.' },
+    { Athlete:'Joey Bosa', Details:'This NFL player came out of ohio state who plays as a defensive end and has a brother who plays the same position for the San Francisco 49ers.'},
+    { Athlete:'Josh Allen', Details:'This NFL player came out Wyoming and currently plays for the Buffalo Bills as a quarterback.'},
+    { Athlete:'Davante Adams', Details:'This NFL player came out of Fresno State and is regarded as one of the best receivers in the NFL.'},
+    { Athlete:'Micah Parsons', Details:'This NFL player came out of Penn State. He now plays for the Dallas Cowboys as a defensive end.'},
+    { Athlete:'Jalen Ramsey', Details:'This NFL player came out of FSU and was recently traded to the Miami Dolphins. He plays as a defensive back.'},
+    { Athlete:'TJ Watt', Details:'This NFL player came out of Wisconsin as a defensive end. currently plays for the pittsburgh steelers.'},
+    { Athlete:'Von Miller', Details:'This NFL player came out of Texas A&M who currently is a defensive end for the Buffalo Bills and has also won a superbowl MVP.'},
+    { Athlete:'Aaron Donald', Details:'This NFL player came out of Pitt as a defensive tackle. He has won one superbowl.'},
+    { Athlete:'Lamar Jackson', Details:'This NFL player came out of Louisville. He plays quarterback for the Baltimore Ravens'},
+    { Athlete:'Derrick Henry', Details:'This NFL player came out of Alabama. He plays running back for the Tennessee Titans.'}
+  )
+
+    guessedAnswer.length = 0
+  startGame()
 }
